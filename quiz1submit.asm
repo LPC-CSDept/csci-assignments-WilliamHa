ccde2ce7bdf01c4bc26fb63cdcb68cd64494737f
AOEpsilia <williamha2010@yahoo.com>
2021-11-30 22:18:25 -0800

    check

--- quiz1submit.asm
+++ quiz1submit.asm
@@ -5,36 +5,35 @@
 # 11/23/2021
 # CSCI-21
 # 
-# Convert the user's Fahrenheit temperature (in integers) to 
-# Celsius(floating point)
 #
 
-      .data
-sub:  .float 32.0
-mul:  .float 5.0
-div:  .float 9.0
-pr:   .asciiz "Enter the Temperature in Fahrenheit: "
-      .text
-      .globl main
+	      .data
+val1:  	.float 32.0
+val2:  	.float 5.0
+val3:	   .float 9.0
+pr:   	.asciiz "Enter the Temperature in Fahrenheit: "
+         .text
+         .globl main
 
-main: jal      getInt               #subroutine gets user input of Fahrenheit in integers
-      sll      $zero, $zero, 0
-      sll      $f2, $v0, 0          #puts the user input into the floating point register
-      cvt.d.w  $f0, $f2             #so that it can be converted into a floating point double 
-      l.d      $f2, sub             #$f2 = 32.0
-      sub.d    $f0, $f0, $f2        #$f0 = userinput - 32.0
-      l.d      $f2, mult            #$f2 = 5.0
-      mul.d    $f0, $f0, $f2        #$f0 = (input - 32.0) * 5.0
-      sll      $zero, $zero, 0
-      sll      $zero, $zero, 0
-      l.d      $f2, div             #$f2 = 9.0
-      div.d    $f0, $f0, $f2
-      sll      $zero, $zero, 0
-      sll      $zero, $zero, 0
-      li       v0, 10               #syscall service code to end program
-      syscall      
+main: 	jal	getInt			   #subroutine gets user input of Fahrenheit in integers
+         sll	$zero, $zero, 0
+         mtc1	$v0, $f1		      #puts the user input into the floating point register
+         cvt.s.w	$f0, $f1		   #so that it can be converted into a floating point double 
+         l.s	$f1, val1		   #$f1 = 32.0
+         sll	$zero, $zero, 0
+         sub.s	$f0, $f0, $f1		#$f0 = userinput - 32.0
+         l.s 	$f1, val2		   #$f1 = 5.0
+         mul.s	$f0, $f0, $f1		#$f0 = (input - 32.0) * 5.0
+         sll	$zero, $zero, 0
+         sll	$zero, $zero, 0
+         l.s 	$f1, val3		   #$f2 = 9.0
+         div.s	$f0, $f0, $f1		
+         sll	$zero, $zero, 0
+         sll	$zero, $zero, 0
+         li	$v0, 10			      #syscall service code to end program
+         syscall      
 
-getInt:  li    $v0, 4
+getInt:	li    $v0, 4
          la    $a0, pr
          syscall
          li    $v0, 5
