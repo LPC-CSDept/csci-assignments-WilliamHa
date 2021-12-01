f75fad9f180950573ad28e0eb091c9aa75dcb747
AOEpsilia <williamha2010@yahoo.com>
2021-11-30 20:14:36 -0800

    check

--- quiz1submit.asm
+++ quiz1submit.asm
@@ -7,10 +7,25 @@
 // 
 //
 
-   .text
-   .globl main
+      .data
+sub:  .float 32.0
+mul:  .float 5.0
+div:  .float 9.0
+pr:   .asciiz "Enter the Temperature in Fahrenheit: "
+      .text
+      .globl main
 
-main: 
-      li $v0, 10
-      syscall        # Never Gonna Give You Up
-                     # Never Gonna Let You Down
+main: jal   getInt
+      sll   $zero, $zero, 0
+      li v0, 10
+      syscall      
+
+getInt:  li $v0, 4
+         la $a0, pr
+         syscall
+         li $v0, 5
+         syscall
+         jr $ra
+         sll $zero, $zero, 0
+
+calcC:   
