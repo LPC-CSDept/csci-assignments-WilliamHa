1cd34e19a9a6d140d4d6f4eada5cf5eb8f374030
AOEpsilia <williamha2010@yahoo.com>
2021-12-17 14:26:36 -0800

    layout corrected

--- Final/q5.asm
+++ Final/q5.asm
@@ -1,3 +1,19 @@
-lui   $t0, 0xffff
-lw    $t1, 0($t0)
-andi  $t1, $t1, 0x0001
+#
+# q5.asm
+#
+# William Ha
+# 12/17/2021
+# Keith Mehl
+# CSCI-21
+#
+
+   .data
+   .text
+   .globl main
+
+main: lui   $t0, 0xffff
+      lw    $t1, 0($t0)
+      andi  $t1, $t1, 0x001
+      beq   $t1, $zero, main
+      lw    $v0,  4($t0)
+      
