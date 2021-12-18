31cba2a1361de035b7267fe00a7a8941ca31dc40
AOEpsilia <williamha2010@yahoo.com>
2021-12-17 16:03:47 -0800

    10 mins

--- Final/q5.asm
+++ Final/q5.asm
@@ -17,12 +17,10 @@ inRe:    lui   $t0, 0xffff
          lw    $t1, 0($t0)          # load from the input control register
          andi  $t1, $t1, 0x001      # reset all bits except lowest order bit
          beq   $t1, $zero, inRe     # if not ready yet, then loop again
-         sll   $zero, $zero, 0
          lw    $v0, 4($t0)          # if input ready, read
 
 outWr:   lw    $t1, 8($t0)
          andi  $t1, $t1, 0x0001
          beq   $t1, $zero, outWr
-         sll   $zero, $zero, 0
          sw    $v0,  12($t0)
 
